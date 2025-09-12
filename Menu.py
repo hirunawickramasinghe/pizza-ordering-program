70d95bf507df14e927d3956d5f4c7721ca0118c8
hirunawickramasinghe <169111636+hirunawickramasinghe@users.noreply.github.com>
2025-09-12 14:16:25 +1200

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,24 @@
+# menu.py
+
+MENU = {
+    "1": ("Cheese", 7.0),
+    "2": ("Pepperoni", 8.0),
+    "3": ("Hawaiian", 10.0),
+    "4": ("Margherita", 6.0),
+    "5": ("Ham", 8.0),
+    "6": ("Veggie Delight", 9.0),
+    "7": ("BBQ Chicken", 11.0),
+    "8": ("Meat Lovers", 12.0),
+    "9": ("Supreme", 13.0),
+    "10": ("Pesto Special", 10.0)
+}
+
+EXTRAS = {
+    "1": ("Extra Cheese", 1.0),
+    "2": ("Mushrooms", 0.5),
+    "3": ("Olives", 0.5),
+    "4": ("Pepperoni", 1.5)
+}
+
+MAX_PIZZAS = 5
+DELIVERY_FEE = 3.0
