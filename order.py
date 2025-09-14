90f916eb1a4b308878765b08ee70a3c9bc0e84cd
hirunawickramasinghe <169111636+hirunawickramasinghe@users.noreply.github.com>
2025-09-14 15:29:11 +1200

    Update order.py

--- order.py
+++ order.py
@@ -9,6 +9,41 @@ from messages import (
     max_reached_message
 )
 
+
+def get_customer_details():
+    print("\nWelcome to Python Pizza! Please enter your details to start your order.")
+
+    name = input("Enter your name: ").strip().title()
+    while name == "":
+        print("Name cannot be blank.")
+        name = input("Enter your name: ").strip().title()
+
+    phone = input("Enter your phone number: ").strip()
+    while phone == "" or not all(c.isdigit() or c in " -+" for c in phone):
+        print("Phone number can only have numbers, spaces, dashes or + sign.")
+        phone = input("Enter your phone number: ").strip()
+
+    order_type = input("Pickup or Delivery? ").strip().lower()
+    while order_type not in ["pickup", "delivery"]:
+        print("Please type 'pickup' or 'delivery'")
+        order_type = input("Pickup or Delivery? ").strip().lower()
+
+    address = ""
+    if order_type == "delivery":
+        address = input("Enter your delivery address: ").strip()
+        while address == "":
+            print("Address cannot be blank.")
+            address = input("Enter your delivery address: ").strip()
+
+    # PAYMENT SECTION
+    payment = input("Payment method (cash/card): ").strip().lower()
+    while payment not in ["cash", "card"]:
+        print("Invalid input. Please type 'cash' or 'card'.")
+        payment = input("Payment method (cash/card): ").strip().lower()
+
+    return {"name": name, "phone": phone, "type": order_type, "address": address, "payment": payment}
+
+
 def take_order():
     order = {}
     total_pizzas = 0
@@ -71,10 +106,11 @@ def take_order():
 
     return order
 
+
 def calculate_total(order, order_type, DELIVERY_FEE):
     total = 0
     for (flavour, extras), qty in order.items():
-        price = MENU[[k for k,v in MENU.items() if v[0]==flavour][0]][1]
+        price = MENU[[k for k, v in MENU.items() if v[0] == flavour][0]][1]
         for extra, price_extra in extras:
             price += price_extra
         total += price * qty
